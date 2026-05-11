6135791faa24e5ca2840cad73b2934e98377dd9c
Pragatheesh <pragatheesh2002@gmail.com>
2026-05-11 18:15:56 +0000

    Update Member Controller

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -1,13 +1,67 @@
-const initializeDb = require('./config/db');
+const db = require("../config/db");
 
+exports.getMembers = (req, res) => {
+  db.all("SELECT * FROM members", [], (err, rows) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
 
-exports.getMember = (request,response) => {
-    initializeDb.all("SELECT * FROM members", (err, rows)=>{
-        if(err){
-            return response.status(500).json(err);
-        }
+    res.json(rows);
+  });
+};
 
-        response.json(rows);
-    })
-    
-}
+exports.addMember = (req, res) => {
+  const { name, plan, joinDate } = req.body;
+
+  db.run(
+    `INSERT INTO members(name, plan, joinDate)
+     VALUES (?, ?, ?)`,
+    [name, plan, joinDate],
+    function (err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        id: this.lastID,
+        message: "Member added",
+      });
+    }
+  );
+};
+
+exports.updateMember = (req, res) => {
+  const { name, plan, joinDate } = req.body;
+
+  db.run(
+    `UPDATE members
+     SET name=?, plan=?, joinDate=?
+     WHERE id=?`,
+    [name, plan, joinDate, req.params.id],
+    function (err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        message: "Member updated",
+      });
+    }
+  );
+};
+
+exports.deleteMember = (req, res) => {
+  db.run(
+    `DELETE FROM members WHERE id=?`,
+    req.params.id,
+    function (err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        message: "Member deleted",
+      });
+    }
+  );
+};
